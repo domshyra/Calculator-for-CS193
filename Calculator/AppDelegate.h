ce534bbcb6ca63b0a176ddf8dab48bcf764c0cb0
Dominic <domshyra@gmail.com>
2012-07-01 11:41:33 -0700

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Dominic Schira on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Dominic Schira on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
